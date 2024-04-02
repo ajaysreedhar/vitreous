66053a074361870087b806408184f5f2d3f1f23e
ajaysreedhar <sree.ajay468@gmail.com>
2024-04-02 21:25:01 +0530

    add: Bootstrap code

---
+++ src/kernel/boot.asm
@@ -0,0 +1,33 @@
+/**
+ * boot.asm - Bootstrap Kernel (ARM Cortex-A72 )
+ * 
+ * Copyright (c) 2024 Ajay Sreedhar.
+ *
+ * Licensed under GNU General Public License 3.0.
+ * See LICENSE file located in the project root for more information.
+ */
+
+.section ".text.boot"
+
+.global _start
+
+_start:
+    mrs     x1, mpidr_el1
+    and     x1, x1, #3
+    cbz     x1, 2f
+
+1:  wfe
+    b       1b
+
+2:  ldr     x1, =_start
+    mov     sp, x1
+    ldr     x1, =__bss_start
+    ldr     w2, =__bss_size
+
+3:  cbz     w2, 4f
+    str     xzr, [x1], #8
+    sub     w2, w2, #1
+    cbnz    w2, 3b
+
+4:  bl      main
+    b       1b
