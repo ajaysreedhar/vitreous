865acbcf44eac5875edd3b89382ac9e84e96316b
ajaysreedhar <sree.ajay468@gmail.com>
2024-04-03 23:09:40 +0530

    add: Read and write function implementation

--- src/kernel/mmio.c
+++ src/kernel/mmio.c
@@ -1,1 +1,18 @@
+/**
+ * mmio.h - Memory mapped IO implementation.
+ * 
+ * Copyright (c) 2024 Ajay Sreedhar.
+ *
+ * Licensed under GNU General Public License 3.0.
+ * See LICENSE file located in the project root for more information.
+ */
+
 #include "mmio.h"
+
+void mmio_write(long address, unsigned int value) {
+    *((volatile unsigned int*) address) = value;
+}
+
+unsigned int mmio_read(long address) {
+    return *((volatile unsigned int*) address);
+}
--- src/kernel/mmio.h
+++ src/kernel/mmio.h
@@ -1,6 +1,18 @@
+/**
+ * mmio.h - Memory mapped IO header.
+ * 
+ * Copyright (c) 2024 Ajay Sreedhar.
+ *
+ * Licensed under GNU General Public License 3.0.
+ * See LICENSE file located in the project root for more information.
+ */
+
 #ifndef VTRS_KERNEL_MMIO_H
 #define VTRS_KERNEL_MMIO_H 1
 
 #define PERIPHERAL_BASE_ADDR 0x0000B880
+
+void mmio_write(long, unsigned int);
+unsigned int mmio_read(long);
 
 #endif 
