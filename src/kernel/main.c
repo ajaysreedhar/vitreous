a53f1622675877c4dcb1020c2e168e077c205e1a
ajaysreedhar <sree.ajay468@gmail.com>
2024-04-07 23:09:45 +0530

    chore: Test draw functionsa

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,4 +1,11 @@
+#include "video.h"
+
 void main() {
+    video_init();
+
+    draw_rect(150,150,400,400,0x03,0);
+    draw_rect(300,300,350,350,0x2e,1);
+
     while (1) {
         // Run infinite loop.
     }
